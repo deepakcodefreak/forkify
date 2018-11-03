8db69d41becd6e59e3a478642ec1ae6ebe694c39
deepakcodefreak <deepak19.engg@gmail.com>
2018-11-03 18:39:17 +0530

    MVC set up for our project

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
-// Global app controller
+import str from './models/Search';
 
-import num from './test'
-const x = 14;
+//import {add as a,multiply as m,ID} from './views/searchview';
 
-console.log(`I imported ${num} from another module and internal variable is ${x}`);
+import * as searchview from './views/searchview'
+
+console.log(`using imported function: ${searchview.add(searchview.ID,2)} and multiply ${searchview.multiply(searchview.ID,2)} ... ${str}` )
+
